09d4a4db62be70da69a3891520475298408546c0
Curtis Tarr <ctarr901@gmail.com>
2022-04-25 23:00:33 +0100

    Fixing ram parsing issue: - Using vm_stat to get RAM info - CPU and RAM handle own parsing - Optimising top command - Adding additional crates for regex parsing - fixes #12

--- src/activity/generator.rs
+++ src/activity/generator.rs
@@ -6,53 +6,31 @@ use crate::activity::ram::RAM;
 
 pub fn get_activity() -> String {
     let top_output = run_top();
-    let output_string = String::from_utf8(top_output.stdout).unwrap();
-    return parse_output_to_activity(output_string);
+    let vm_stat_output = run_vm_stat();
+
+    let top_output_string = String::from_utf8(top_output.stdout).unwrap();
+    let vm_stat_output_string = String::from_utf8(vm_stat_output.stdout).unwrap();
+
+    let cpu = CPU::from_str(&*top_output_string).unwrap();
+    let ram = RAM::from_str(&*vm_stat_output_string).unwrap();
+
+    return format!("{:.2}% {:.2}GB", cpu.get_usage(), ram.get_gb_used());
 }
 
 fn run_top() -> Output {
     return Command::new("top")
         .arg("-l")
         .arg("1")
+        .arg("-s")
+        .arg("0")
+        .arg("-n")
+        .arg("0")
         .output()
         .unwrap_or_else(|e| { panic!("failed to execute top: {}", e) });
 }
 
-fn parse_output_to_activity(output_string: String) -> String {
-    let lines = output_string.split("\n");
-
-    let mut cpu_found = false;
-    let mut ram_found = false;
-
-    let mut cpu = CPU { user: 0.00, system: 0.00, idle: 0.00 };
-    let mut ram = RAM { used: 0, wired: 0, unused: 0 };
-
-    for s in lines {
-        if cpu_found && ram_found {
-            break;
-        }
-
-        if s.starts_with("CPU usage") {
-            cpu = CPU::from_str(s).unwrap();
-            cpu_found = true;
-        } else if s.starts_with("PhysMem") {
-            ram = RAM::from_str(s).unwrap();
-            ram_found = true;
-        }
-    }
-
-    return format!("{:.2}% {}MB", cpu.user + cpu.system, ram.used);
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_parse_top_to_activity() {
-        let output = String::from("CPU usage: 8.82% user, 14.70% sys, 76.47% idle\nPhysMem: 7515M used (1430M wired), 135M unused.");
-        let activity = parse_output_to_activity(output);
-
-        assert_eq!(activity, String::from("23.52% 7515MB"));
-    }
+fn run_vm_stat() -> Output {
+    return Command::new("vm_stat")
+        .output()
+        .unwrap_or_else(|e| { panic!("failed to execute vm_stat: {}", e) });
 }
